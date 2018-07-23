20ae8b27ae2af6681a7f945e2386783d0de2abd6
Jose Bernat <jbf@iesat.com>
2018-07-23 22:24:55 +0200

    feat: INDEX

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const express = require ('express') //
+const express = require ('express')
+const path = require('path');
+const indexRouter = require('./routes/index.routes')
+//
 // We create our own server named app
 // Express server handling requests and responses
 const app = express();
@@ -9,8 +12,12 @@ app.set('view engine', 'hbs');
 
 require('./configs/db.config');
 
-
+app.use('/', indexRouter)
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000);
+
+module.exports = app
 
 
 
