d04209c36e3f20310b27ef84ee547c7eeffdcc5e
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-07-24 19:15:58 +0200

    This is to test #1

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
+require('dotenv').config(); 
+
+const createError = require ('http-errors')
 const express = require ('express')
 const path = require('path');
-const indexRouter = require('./routes/index.routes')
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+
+require('./configs/db.config');
+require('./configs/hbs.config');
+require('./configs/passport.config').setup(passport);
+
+//Routers
+const indexRouter = require('./routes/index.routes');
+const usersRouter = require('./routes/user.routes'); 
+const sessionsRouter = require('./routes/sessions.routes'); 
 //
 // We create our own server named app
 // Express server handling requests and responses
@@ -10,10 +25,51 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-require('./configs/db.config');
 
-app.use('/', indexRouter)
+app.use(logger('dev')); 
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false}));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session( {
+  secret: 'SuperFookingSecret, (fucking change me)', 
+  resave: false, 
+  saveUninitialized: true, 
+  cookie: {
+    secure: false, 
+    httpOnly: true, 
+    maxAge: 60 * 60 * 24 * 1000
+  }
+
+})); 
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
+app.use((req, res, next) => {
+  res.locals.session = req.user; 
+  next(); 
+})
+
+app.use('/users', usersRouter);
+app.use('/sessions', sessionsRouter); 
+app.use('/', indexRouter)
+
+//catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404)); 
+}); 
+
+//error handler
+app.use(function(err, req, res, next) {
+  //set locals to provide errors only during development
+  res.locals.message = err.message; 
+  res.locals.error = req.app.get('env') === 'development' ? err: {};  
+
+  //render tue error page
+  res.status(err.status || 500 )
+})
+
 
 app.listen(3000);
 
