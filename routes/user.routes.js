23be5528ef28db679e144cdda1d1b6a9dc57c78b
Octavio <s.octavio90@gmail.comgit config --global user.email s.octavio90@gmail.comgit congfig --global user.name s.octavio90@gmail.comgit config --global user.name s.octavio90@gmail.com>
2018-07-24 17:11:56 +0200

    Feat: login in progress (users controller, auth middleware, passport local)

---
+++ routes/user.routes.js
@@ -0,0 +1,4 @@
+const constants = require('../constants'); 
+const express = require('express'); 
+const router = express.Router(); 
+const usersController = require('../controllers/users.controller')
