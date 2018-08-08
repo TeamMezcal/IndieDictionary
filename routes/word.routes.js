bf1aed4f67b51becabba7c84f4b4628a579c4fbe
Jose Bernat <jbf@iesat.com>
2018-08-08 12:22:31 +0200

    fix: added auth middleware to word like

--- routes/word.routes.js
+++ routes/word.routes.js
@@ -13,7 +13,7 @@ router.get("/create", authMiddleware.isAuthenticated, wordsController.create);
 router.post("/create", authMiddleware.isAuthenticated, wordsController.doCreate); 
 
 router.get('/:id', wordsController.get); 
-router.get("/:id/likes", wordsController.doLike)
+router.get("/:id/likes",authMiddleware.isAuthenticated, wordsController.doLike)
 //router.get('/5b65b7295d541324bd6b36d9', wordsController.random); 
 
 // router.get('/:id/edit', authMiddleware.isAuthenticated, wordsController.edit)
